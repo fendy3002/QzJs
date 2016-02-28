1c37a8f1493cee448aa7eb48a13bb139b17dc4f0
ricky.sebastian <ricky.sebastian@cakrastudio.com>
2016-02-28 15:06:41 +0700

    Moved several web function to core

--- src/js/core.js
+++ src/js/core.js
@@ -7,6 +7,7 @@ Qz.Linq = Qz.Linq || {};
 Qz.Math = Qz.Math || {};
 Qz.String = Qz.String || {};
 Qz.Url = Qz.Url|| {};
+Qz.Web = Qz.Web || {};
 
 var Q = Q || {};
 Q.Z = Q.Z || {};
@@ -356,6 +357,201 @@ Q.Z = Q.Z || {};
     // section Qz.Url.domainUrl
     root.domainUrl = Qz.Object.property();
 }(Qz.Url, jQuery));
+
+// section Qz.Web
+(function (root, $) {
+    "use strict";
+    // section Qz.Web.surpressBrowserShortcut
+    (function (root, $) {
+        var surpressBrowserShortcut = function (config) {
+            var option = $.extend(Qz.Context.browserShortcutList(), config);
+
+    		var onKeyDown = function(e){
+    			if(Qz.Context.overrideBrowserShortcut())
+    			{
+    				if(option.ctrlF && e.ctrlKey && e.keyCode == 70){
+    					e.preventDefault();
+    				}
+    				else if (option.ctrlI && e.ctrlKey && e.keyCode == 73) {
+    				    e.preventDefault();
+    				}
+    				else if (option.ctrlN && e.ctrlKey && e.keyCode == 78) {
+    					e.preventDefault();
+    				}
+    				else if (option.ctrlO && e.ctrlKey && e.keyCode == 79) {
+    					e.preventDefault();
+    				}
+    				else if (option.ctrlR && e.ctrlKey && e.keyCode == 82) {
+    					e.preventDefault();
+    				}
+    				else if (option.ctrlS && e.ctrlKey && e.keyCode == 83) {
+    				    e.preventDefault();
+    				}
+    				else if (option.f1 && e.keyCode == 112) {
+    				    e.preventDefault();
+    				}
+    				else if (option.f2 && e.keyCode == 113) {
+    					e.preventDefault();
+    				}
+    				else if (option.f3 && e.keyCode == 114) {
+    					e.preventDefault();
+    				}
+    				else if (option.f4 && e.keyCode == 114) {
+    				    e.preventDefault();
+    				}
+    			}
+    		};
+    		$(document).off("keydown.globalKeyDown");
+    		$(document).on("keydown.globalKeyDown", onKeyDown);
+        };
+
+        root.surpressBrowserShortcut = surpressBrowserShortcut;
+    }(root, $));
+
+    // section Qz.Web.lastKeyPress
+    root.lastKeyPress = Qz.Object.property();
+
+    // section Qz.Web.globalKeyUp
+    (function (root, $) {
+        var events = [];
+    	var eventsEnum = Qz.Linq.enums(events);
+    	root.globalKeyUp = function (handler) {
+    		var functionExists = eventsEnum.any(function(k){ return Qz.Func.compare(handler, k.handler); });
+    		if(!functionExists){
+    			var key = Qz.Math.randomInt(0, 10000);
+    			while(eventsEnum.any(function(k){ return k.key == key; })){
+    				key = Qz.Math.randomInt(0, 10000);
+    			}
+    			events.push({key: key,
+    				handler: handler});
+    		}
+    		else{
+    			console.log("function " + handler + " already registered");
+    		}
+        };
+
+    	var onKeyUp = function(e){
+    		root.lastKeyPress({
+    			keyCode : e.keyCode,
+    			shiftKey: e.shiftKey,
+    			ctrlKey: e.ctrlKey
+    		});
+    		for(var i=0; i < events.length; i++){
+    			events[i].handler.apply(this, [e]);
+    		}
+    	};
+    	$(document).on("keyup", onKeyUp);
+    }(root, $));
+
+    // source: http://stackoverflow.com/questions/2897155/get-cursor-position-in-characters-within-a-text-input-field
+    // section Qz.Web.getCaretPosition
+	root.getCaretPosition = function (oField) {
+		// Initialize
+		var iCaretPos = 0;
+
+		// IE Support
+		if (document.selection) {
+			// Set focus on the element
+			oField.focus();
+			// To get cursor position, get empty selection range
+			var oSel = document.selection.createRange();
+			// Move selection start to 0 position
+			oSel.moveStart('character', -oField.value.length);
+			// The caret position is selection length
+			iCaretPos = oSel.text.length;
+		}
+
+		// Firefox support
+		else if (oField.selectionStart || oField.selectionStart == '0'){
+			iCaretPos = oField.selectionStart;
+		}
+
+		// Return results
+		return iCaretPos;
+	}
+
+    // section Qz.Web.isCaretPositionLast
+	root.isCaretPositionLast = function (oField) {
+		return root.getCaretPosition(oField) == oField.value.length;
+	};
+
+    // section Qz.Web.resolveTarget
+    root.resolveTarget = function ($startElement, target) {
+        var $computedTarget;
+        if (target == 'this') {
+            $computedTarget = $startElement;
+        }
+        else {
+            try {
+                $computedTarget = new Function('elem', "return $($(elem)." + target + ");")($startElement);
+            } catch (ex) {
+                $computedTarget = $startElement.find(target);
+            }
+        }
+        return $computedTarget;
+    };
+
+    // section Qz.Web.scopeTab
+    (function (root, $) {
+    	var focus = function($elem){
+    		if($elem.is('.select2-hidden-accessible')){
+    			var $focus = $elem.next('.select2-container').find('.select2-selection');
+    			$focus.focus();
+    			$focus.on('focus');
+    		}
+    		else{
+    			$elem.focus();
+    			$elem.on('focus');
+    		}
+    	};
+
+    	var handleCrossForm = function($form, incremental){
+    		var $allForm = $("form:has([data-tab-first])");
+    		var nextIndex = $allForm.index($form);
+    		if(nextIndex == $allForm.length - incremental){
+    			nextIndex = 0;
+    		}
+    		else{
+    			nextIndex += incremental;
+    		}
+    		var $focusElement = $("form").eq(nextIndex).find('[data-tab-first]');
+    		focus($focusElement);
+    	}
+        root.scopeTab = function (elem) {
+    		var $form = $(elem);
+    		$form.on('keydown', '*', function(e){
+                if (!e.ctrlKey && (e.keyCode == 9 || e.which == 9)) {
+    				var lastKeyPress = Qz.Web.lastKeyPress();
+    				if(lastKeyPress.keyCode == 120 && !e.shiftKey) // F9
+    				{
+    					e.preventDefault();
+    					handleCrossForm($form, 1);
+    				}
+    				else if(lastKeyPress.keyCode == 120 && e.shiftKey) // F9
+    				{
+    					e.preventDefault();
+    					handleCrossForm($form, -1);
+    				}
+
+    				var $this = $(this);
+    				if($this.is('.select2.select2-container')){
+    					$this = $this.prev('.select2-hidden-accessible');
+    				}
+
+    				if(!e.shiftKey && $this.is('[data-tab-last]')){
+    					e.preventDefault();
+    					focus($form.find('[data-tab-first]'));
+    				}
+    				else if(e.shiftKey && $this.is('[data-tab-first]')){
+    					e.preventDefault();
+    					focus($form.find('[data-tab-last]'));
+    				}
+    			}
+    		});
+    	};
+    }(root, $));
+}(Qz.Web, jQuery));
+
 
 // section Q.Z
 (function (root, $) {
